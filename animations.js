b9a9ccb55b6343784e3f397487fa6f5001656cd1
ShagantiNagasree <153216600+ShagantiNagasree@users.noreply.github.com>
2024-10-15 18:13:59 +0530

    Add files via upload

---
+++ animations.js
@@ -0,0 +1,4 @@
+document.getElementById('animateButton').addEventListener('click', function() {
+    const box = document.getElementById('animateBox');
+    box.classList.toggle('visible'); // Toggle the animation class
+});
